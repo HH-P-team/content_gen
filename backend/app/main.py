5018faa5dcf51e239fb52d53e98b33945f4a754d
homutovan <andrey.khomutov@rt.ru>
2024-05-19 00:19:02 +0700

    fix bugs

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
-from core.routers import router
+from app.core.routers import router
 
 origins = [
     "*",
