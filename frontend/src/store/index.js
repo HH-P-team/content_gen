f82be4396915a1352b49ab63cd647a7c43ba77d2
homutovan <andrey.khomutov@rt.ru>
2024-05-22 04:46:11 +0700

    add reducer

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,29 +1,92 @@
 import { configureStore } from '@reduxjs/toolkit';
-import Cookie from 'js-cookie';
+// import Cookie from 'js-cookie';
+
+// const mainReducer = (
+// 	prevState = {
+// 		user: {
+// 			role: Cookie.get('role') || 'admin'
+// 		}
+// 	},
+// 	action
+// ) => {
+// 	const { type, payload } = action;
+
+// 	switch (type) {
+// 		case 'SET_ROLE':
+// 			Cookie.set('role', payload);
+// 			return {
+// 				...prevState,
+// 				user: {
+// 					...prevState.user,
+// 					role: payload
+// 				}
+// 			};
+// 		default:
+// 			return prevState;
+// 	}
+// };
+
+// export default configureStore({ reducer: mainReducer });
 
 const mainReducer = (
 	prevState = {
-		user: {
-			role: Cookie.get('role') || 'admin'
-		}
+		buttonState: 1,
+		buttonState1: 2,
+		addSubjectMenuState: false,
+		subjects: [],
+		products: {},
+		posts: {},
 	},
 	action
 ) => {
-	const { type, payload } = action;
+	const { type, value } = action;
 
 	switch (type) {
-		case 'SET_ROLE':
-			Cookie.set('role', payload);
+		case 'CASE_SUBJECT_MENUSTATE':
 			return {
 				...prevState,
-				user: {
-					...prevState.user,
-					role: payload
+				addSubjectMenuState: value,
+			};
+
+		case 'CASE_ADD_SUBJECTS':
+			return {
+				...prevState,
+				subjects: [...value, ...prevState.subjects],
+			};
+
+		case 'CASE_SET_SUBJECTS':
+			return {
+				...prevState,
+				subjects: value,	
+			};
+
+		case 'CASE_ADD_PRODUCT':
+			var { subjectId, product} = value;
+
+			const prevProduct = prevState.products[subjectId] ? prevState.products[subjectId] : []
+
+			return {
+				...prevState,
+				products: {
+					...prevState.products,
+					...{[subjectId]: [...prevProduct, ...[product]]},
 				}
 			};
+	
+		case 'CASE_SET_PRODUCTS':
+			var { subjectId, product} = value;
+			
+			return {
+				...prevState,
+				products: {
+					...prevState.products,
+					...{[subjectId]: product},
+				}
+			};
+
 		default:
 			return prevState;
 	}
-};
+}
 
 export default configureStore({ reducer: mainReducer });
