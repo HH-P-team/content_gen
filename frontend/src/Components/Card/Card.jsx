2a7517f9663a4d8e54f85d8392d9148289f706c3
homutovan <andrey.khomutov@rt.ru>
2024-05-22 04:47:29 +0700

    update components

--- frontend/src/Components/Card/Card.jsx
+++ frontend/src/Components/Card/Card.jsx
@@ -1,12 +1,23 @@
 import './Card.css';
 import ActiveImage from '../ActiveImage/ActiveImage';
+import { useEffect, useState } from 'react';
 
 export default function Card(props) {
-    console.log(props);
+
+    const [data, setData] = useState('');
+
+    useEffect(() => {
+        props.request(props.params).then((data) => {
+            if (data.status) {
+                setData(data.result);
+            }
+        });
+    }, []);
+    
     return (
         <div className="Card" data-id={props.id}>
             <div className='ImageWrapper'>
-                <ActiveImage message={props.name}/>
+                <ActiveImage data={data}/>
             </div>
             <div>{props.name}</div>
         </div>
